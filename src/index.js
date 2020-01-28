a7c9210e96a799b0639401b1317da15948d4844b
Alexander Avakov <yaavakov@gmail.com>
2020-01-28 09:37:03 +0300

    Rename request to response and use ok flag to determine status.

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ const {CI_COMMIT_REF_NAME, SONAR_HOST, SONAR_LOGIN, SONAR_PROJECT} = process.env
         component: SONAR_PROJECT,
         metricKeys: 'coverage',
     });
-    const request = await fetch(`${SONAR_HOST}/api/measures/component?${query}`, {
+    const response = await fetch(`${SONAR_HOST}/api/measures/component?${query}`, {
         headers: {Authentication: `Basic ${SONAR_LOGIN}`},
     });
-    const data = await request.json();
+    const data = await response.json();
 
-    if (request.status !== 200) {
+    if (!response.ok) {
         const error = data.errors.map(({msg}) => msg).join('\n');
 
         console.log(`SonarQube request failed:\n${error}`);
