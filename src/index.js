e6a580aeabe3fcbb0ea1cd20f73ee73aa206e220
Alexander Avakov <yaavakov@gmail.com>
2020-04-17 11:33:22 +0300

    Add more SonarQube metrics

--- src/index.js
+++ src/index.js
@@ -5,11 +5,26 @@ const querystring = require('querystring');
 
 const {CI_COMMIT_REF_NAME, SONAR_HOST, SONAR_TOKEN, SONAR_PROJECT} = process.env;
 
+const SONAR_METRICS = [
+    'coverage',
+    'bugs',
+    'vulnerabilities',
+    'security_hotspots',
+    'code_smells',
+    'branch_coverage',
+    'conditions_to_cover',
+    'uncovered_conditions',
+    'line_coverage',
+    'lines_to_cover',
+    'uncovered_lines',
+    'new_coverage',
+];
+
 (async () => {
     const query = querystring.stringify({
         branch: CI_COMMIT_REF_NAME,
         component: SONAR_PROJECT,
-        metricKeys: 'coverage',
+        metricKeys: SONAR_METRICS.join(','),
     });
     const response = await fetch(`${SONAR_HOST}/api/measures/component?${query}`, {
         headers: {Authentication: `Basic ${SONAR_TOKEN}`},
@@ -24,5 +39,16 @@ const {CI_COMMIT_REF_NAME, SONAR_HOST, SONAR_TOKEN, SONAR_PROJECT} = process.env
         return;
     }
 
-    console.log(`OVERALL COVERAGE: ${data.component.measures[0].value}%`);
+    const measures = component.measures.reduce((carry, current) => {
+        const metricName = `sonar_metric_${current.metric}`;
+        if (current.value !== undefined) {
+            carry.push(`${metricName} ${current.value}`);
+        } else if (current.periods) {
+            carry.push(`${metricName} ${Number(current.periods[0].value).toFixed(2)}`);
+        }
+
+        return carry;
+    }, []).join('\n');
+
+    console.log(`\nSonarQube statistics:\n${measures}\n`);
 })();
